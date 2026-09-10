9057c36eb652ce2583fc5f9b5842f10db4326f85
tgwalehm-byte <tgwalehm@gmail.com>
2026-09-10 20:26:47 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = int(os.environ["API_ID"])
+API_HASH = os.environ["API_HASH"]
+
+BOT_TOKEN = os.environ["BOT_TOKEN"]
+SESSION_STRING = os.environ["SESSION_STRING"]
+
+MONGO_URL = os.environ["MONGO_URL"]
+LOG_CHANNEL = int(os.environ["LOG_CHANNEL"])
+
+POLL_SECONDS = int(os.getenv("POLL_SECONDS", "3"))
